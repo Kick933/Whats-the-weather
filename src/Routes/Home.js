8cfb1d47c50bfa64f0952bfa13077fd991fa981a
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-11-23 15:04:01 +0530

    Changed background.

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -5,7 +5,7 @@ import Result from "../components/Result";
 function Home({ setPlace, place, deleteResult }) {
     document.title = "What's the Weather?"
     return (
-        <div className='min-h-screen w-full overflow-y-hidden bg-white dark:bg-gray-900 dark:text-white'>
+        <div className='min-h-screen w-full overflow-y-hidden bg-gray-100 dark:bg-gray-900 dark:text-white'>
             <Search setPlace={setPlace} place={place} />
             <Result place={place} deleteResult={deleteResult} />
         </div>
