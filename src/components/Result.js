954f9be5ff35c269ed05c00d85725828083d6362
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-12-06 20:56:44 +0530

    Changed Unique key for components

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,9 +4,9 @@ import ResultCard from './ResultCard'
 function Result({ place, deleteResult }) {
     return (
         <div className='flex flex-row gap-8 p-2 w-screen justify-center flex-wrap'>
-            {place.length ? place.map(place => <ResultCard key={place} place={place} deleteResult={deleteResult} />) : <p className="m-16 sm:mx-auto text-center text-xl text-indigo-400">No Location added. Search for your location now.</p>}
+            {place.length ? place.map(place => <ResultCard key={place.searchName} place={place} deleteResult={deleteResult} />) : <p className="m-16 sm:mx-auto text-center text-xl text-indigo-400">No Location added. Search for your location now.</p>}
         </div>
     )
 }
 
-export default Result
+export default React.memo(Result)
