6679226bf304a8948247d4ff6b9653f9672b7987
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-12-06 20:57:23 +0530

    Created data fetching functions.

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -1,9 +1,5 @@
 export const getWeather = async (place) => {
-    try {
-        const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${process.env.REACT_APP_KEY}&units=metric`)
-        const data = await rawData.json()
-        return data
-    } catch (e) {
-        console.log(e.message)
-    }
+    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${process.env.REACT_APP_KEY}&units=metric`)
+    const data = await rawData.json()
+    return data
 }
