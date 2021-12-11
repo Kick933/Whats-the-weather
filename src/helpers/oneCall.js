38aac9eaa3c70471eef412aec3810fe2fe955604
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-12-11 16:50:08 +0530

    Metric units will be fetched from now onwards.

--- src/helpers/oneCall.js
+++ src/helpers/oneCall.js
@@ -1,5 +1,5 @@
 export const fetchOneCall = async (lat, lon) => {
-    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_KEY}`)
+    const rawData = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_KEY}&units=metric`)
     const data = await rawData.json()
     return data
 }
