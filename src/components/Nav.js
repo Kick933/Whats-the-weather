98df78da7187ecae01e44318af33dc3d4cbd4b81
Pavitar Kumar <pavitrsheoran003@gmail.com>
2022-05-02 16:08:41 +0530

    Added dark mode icon.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {FiMoon} from 'react-icons/fi'
 function Nav() {
 
     function changeTheme() {
@@ -14,7 +15,7 @@ function Nav() {
     return (
         <div className="bg-indigo-300 dark:bg-gray-800 dark:opacity-90 justify-between items-center px-4  flex h-16 dark:text-gray-50 text-gray-900">
             <h1 className="text-center text-xl font-bold font-serif"><Link to="/">What's The Weather</Link></h1>
-            <div onClick={() => changeTheme()} className="cursor-pointer bg-black dark:bg-yellow-400 w-8 h-8 rounded-full"></div>
+            <div onClick={() => changeTheme()} className="transition-delay-400 transition-all cursor-pointer text-3xl text-black dark:text-yellow-400 w-8 h-8"><FiMoon /></div>
         </div>
     )
 }
